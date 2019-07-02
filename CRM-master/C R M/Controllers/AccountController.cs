aefa3c4d0f3d8a3f1233c854de23f91709b16a37
dabakura <38744169+dabakura@users.noreply.github.com>
2019-07-02 16:22:35 -0600

    Avance 1

--- CRM-master/C R M/Controllers/AccountController.cs
+++ CRM-master/C R M/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace C_R_M.Controllers
---
+++ CRM-master/C R M/Controllers/Email.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace C_R_M.Controllers
+{
+    public class Email
+    {
+        
+    }
+}
